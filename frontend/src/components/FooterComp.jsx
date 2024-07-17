f5324dbd0d305706e8a8a31d29d9c04bc835f2ff
vivek1504 <jadhavvivek2743@gmail.com>
2024-07-17 21:14:58 +0530

    footer

---
+++ frontend/src/components/FooterComp.jsx
@@ -0,0 +1,23 @@
+export const FooterComp = () => {
+    return  <footer className="bg-slate-300 rounded-lg shadow mt-4 bottom-0">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-black sm:text-center">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-black sm:mt-0">
+            <li>
+                <a href="#" className="hover:underline me-4 md:me-6">About</a>
+            </li>
+            <li>
+                <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+            </li>
+            <li>
+                <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+            </li>
+            <li>
+                <a href="#" className="hover:underline">Contact</a>
+            </li>
+        </ul>
+        </div>
+    </footer>
+    
+}
