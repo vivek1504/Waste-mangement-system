a64b503301d51919a231bfde42e7162065254cc5
dubeanant <dubeyarjun1071977@gmail.com>
2024-07-17 14:26:50 +0530

    added_user_page

--- frontend/src/components/userTab/userCamera/Camera.jsx
+++ frontend/src/components/userTab/userCamera/Camera.jsx
@@ -52,23 +52,24 @@ const Camera = () => {
   };
 
   return (
-    <div className='border-2'>
-      <div className='flex items-center justify-center pt-10'>
+    <div>
+      <div className='flex justify-center items-center pt-4'>
         <Webcam
           audio={false}
           ref={webcamRef}
           screenshotFormat="image/jpeg"
-          width="25%"
-          height="25%"
-          className='rounded-lg'
+          width="27%"
+          height="32%"
+          className='rounded-lg '
         />
         {imgSrc && (
-          <div className='pl-8'>
-            <img className='rounded-lg' src={imgSrc} alt="Captured" />
+          <div className='ml-10 h-full w-full'>
+            <img className='rounded-lg ' src={imgSrc} alt="Captured" />
           </div>
         )}
       </div>
 
+      <div className='flex items-center justify-center mt-4 gap-3' >
       <div className='flex items-center justify-center pt-2'>
         <button className='border-2 rounded-lg dark:bg-black dark:text-white h-12 w-40' onClick={capture}>Capture photo</button>
       </div>
@@ -78,10 +79,11 @@ const Camera = () => {
           <button className='border-2 rounded-lg dark:bg-black dark:text-white h-12 w-40' onClick={uploadImage}>Upload photo</button>
         </div>
       )}
+      </div>
 
       <div className="inline-flex items-center justify-center w-full">
         <hr className="w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"/>
-        <span className="absolute px-3 text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-black text-xl font-semibold">OR</span>
+        <span className="absolute px-3 text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-black text-xl font-semibold mt-4">OR</span>
       </div>
 
       <FileUploadComponent />
--- frontend/src/components/userTab/userCamera/UploadImg.jsx
+++ frontend/src/components/userTab/userCamera/UploadImg.jsx
@@ -46,17 +46,19 @@ const FileUploadComponent = () => {
 
   return (
     <div className="flex items-center justify-center">
-      <div className="sm:max-w-lg w-full p-10 bg-white rounded-xl z-10">
+      <div className="sm:max-w-lg w-50  bg-white rounded-xl z-10">
         <form className="space-y-3" onSubmit={handleSubmit}>
           <div className="grid grid-cols-1 space-y-2">
             <label className="text-sm font-bold text-gray-500 tracking-wide">Attach Document</label>
             <div className="flex items-center justify-center w-full">
-              <label className="flex flex-col rounded-lg border-4 border-dashed w-full h-60 p-10 group text-center">
+              <label className="flex flex-col rounded-lg border-4 border-dashed w-full h-40 p-10 group text-center">
                 <div className="h-full w-full text-center flex flex-col items-center justify-center">
-                  <div className="flex flex-auto max-h-48 w-2/5 mx-auto -mt-10">
-                    <img className="has-mask h-36 object-center" src="https://img.freepik.com/free-vector/image-upload-concept-landing-page_52683-27130.jpg?size=338&ext=jpg" alt="Upload" />
-                  </div>
+                  {/* <div className="flex flex-auto max-h-48 w-2/5 mx-auto -mt-10">
+                    <img className="has-mask h-20 object-center" src="https://img.freepik.com/free-vector/image-upload-concept-landing-page_52683-27130.jpg?size=338&ext=jpg" alt="Upload" />
+                  </div> */}
+                  <div className='mt-20'>
                   <p className="pointer-none text-gray-500"><span className="text-sm">Drag and drop</span> files here <br /> or <a href="#" onClick={handleFileInputClick} className="text-blue-600 hover:underline">select a file</a> from your computer</p>
+                  </div>
                 </div>
                 <input ref={fileInputRef} type="file" className="hidden" onChange={handleFileChange} />
               </label>
@@ -73,7 +75,7 @@ const FileUploadComponent = () => {
             </div>
           )}
           <div>
-            <button type="submit" className="my-5 w-full flex justify-center bg-blue-500 text-gray-100 p-4 rounded-full tracking-wide
+            <button type="submit" className="my-5 h-14 w-full flex justify-center items-center bg-blue-500 text-gray-100 p-4 rounded-full tracking-wide
                 font-semibold focus:outline-none focus:shadow-outline hover:bg-blue-600 shadow-lg cursor-pointer transition ease-in duration-300">
               Upload
             </button>
